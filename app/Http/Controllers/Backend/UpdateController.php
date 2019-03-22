bb52afb6f4956ed029fb6a50b450ac6ed9db0756
Hardik Tankariya <hardik.tankariya@gmail.com>
2019-03-22 14:53:22 +0530

    fix before laravel backup

--- app/Http/Controllers/Backend/UpdateController.php
+++ app/Http/Controllers/Backend/UpdateController.php
@@ -9,45 +9,47 @@ use Illuminate\Support\Facades\Storage;
 
 class UpdateController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('backend.update.index');
     }
 
-   public function listFiles(Request $request){
-        $this->validate($request,[
-            'file'=> 'required|mimes:zip'
+    public function listFiles(Request $request)
+    {
+        $this->validate($request, [
+            'file' => 'required|mimes:zip'
         ]);
-       $file = $request->file('file');
-       $file_name = time().'_'.$file->getClientOriginalName();
-       $file->move(public_path().'/updates/' , $file_name);
-       $is_verified = false;
-       $checkFiles =  \Zipper::make(public_path().'/updates/'.$file_name)->listFiles('/\.key/i');
-       foreach ($checkFiles as $item){
-           if($item == md5('NeonLMSUpdate').'.key'){
-               $is_verified = true;
-           }
-       }
-       if($is_verified == true){
-           $files =  \Zipper::make(public_path().'/updates/'.$file_name)->listFiles();
-           return view('backend.update.file-list',compact('files','file_name'));
-       }else{
-           unlink(public_path().'/updates/'.$file_name);
-           return redirect(route('admin.update-theme'))->withFlashDanger(__('alerts.backend.general.unverified'));
-       }
+        $file = $request->file('file');
+        $file_name = time() . '_' . $file->getClientOriginalName();
+        $file->move(public_path() . '/updates/', $file_name);
+        $is_verified = false;
+        $checkFiles = \Zipper::make(public_path() . '/updates/' . $file_name)->listFiles('/\.key/i');
+        foreach ($checkFiles as $item) {
+            if ($item == md5('NeonLMSUpdate') . '.key') {
+                $is_verified = true;
+            }
+        }
+        if ($is_verified == true) {
+            $files = \Zipper::make(public_path() . '/updates/' . $file_name)->listFiles();
+            return view('backend.update.file-list', compact('files', 'file_name'));
+        } else {
+            unlink(public_path() . '/updates/' . $file_name);
+            return redirect(route('admin.update-theme'))->withFlashDanger(__('alerts.backend.general.unverified'));
+        }
 
-   }
+    }
 
-   public function updateTheme(Request $request){
-       $file_name = $request->file_name;
-      if($request->submit == 'cancel'){
-          unlink(public_path().'/updates/'.$file_name);
-         return redirect(route('admin.update-theme'))->withFlashDanger(__('alerts.backend.general.cancelled'));
-      }else{
+    public function updateTheme(Request $request)
+    {
+        $file_name = $request->file_name;
+        if ($request->submit == 'cancel') {
+            unlink(public_path() . '/updates/' . $file_name);
+            return redirect(route('admin.update-theme'))->withFlashDanger(__('alerts.backend.general.cancelled'));
+        } else {
+            \Zipper::make(public_path() . '/updates/' . $file_name)->extractTo(base_path());
+            unlink(public_path() . '/updates/' . $file_name);
 
-          \Zipper::make(public_path().'/updates/'.$file_name)->extractTo(base_path());
-          unlink(public_path().'/updates/'.$file_name);
-
-          return redirect(route('admin.update-theme'))->withFlashSuccess(__('alerts.backend.general.updated'));
-      }
-   }
+            return redirect(route('admin.update-theme'))->withFlashSuccess(__('alerts.backend.general.updated'));
+        }
+    }
 }
