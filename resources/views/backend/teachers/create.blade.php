2758e1308a09c3b04ca3b7f7335d6fa16ef9ced4
Hardik Tankariya <hardik.tankariya@gmail.com>
2019-07-12 16:09:48 +0530

    account, timer fixes after 2.1.3

--- resources/views/backend/teachers/create.blade.php
+++ resources/views/backend/teachers/create.blade.php
@@ -7,11 +7,10 @@
     <div class="card">
         <div class="card-header">
             <h3 class="page-title d-inline">@lang('labels.backend.teachers.create')</h3>
-                <div class="float-right">
-                    <a href="{{ route('admin.teachers.index') }}"
-                       class="btn btn-success">@lang('labels.backend.teachers.view')</a>
-
-                </div>
+            <div class="float-right">
+                <a href="{{ route('admin.teachers.index') }}"
+                   class="btn btn-success">@lang('labels.backend.teachers.view')</a>
+            </div>
         </div>
         <div class="card-body">
             <div class="row">
