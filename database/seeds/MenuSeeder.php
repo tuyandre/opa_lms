e952b9fd24965e6e9dae8919132775b225e78abf
Hardik Tankariya <hardik.tankariya@gmail.com>
2019-07-27 11:31:19 +0530

    Menu seeder updated

--- database/seeds/MenuSeeder.php
+++ database/seeds/MenuSeeder.php
@@ -64,5 +64,27 @@ class MenuSeeder extends Seeder
         $nav_menu_config->value = $nav_menu->id;
         $nav_menu_config->save();
 
+
+        $menus = \Harimayco\Menu\Models\Menus::all();
+        foreach ($menus as $menu) {
+            if ($menu != NULL) {
+                $menuItems = \Harimayco\Menu\Models\MenuItems::where('menu', '=', $menu->id)->get();
+                if ($menuItems != null) {
+                    $allMenu = [];
+                    foreach ($menuItems as $item) {
+                        $allMenu[str_slug($item['label'])] = $item['label'];
+                    }
+                    $main[str_slug($menu->name)] = $allMenu;
+                    $file = fopen(public_path('../resources/lang/en/custom-menu.php'), 'a');
+                    if ($file !== false) {
+                        ftruncate($file, 0);
+                    }
+                    fwrite($file, '<?php return ' . var_export($main, true) . ';');
+
+                    Artisan::call('menu:import');
+                }
+            }
+        }
+
     }
 }
