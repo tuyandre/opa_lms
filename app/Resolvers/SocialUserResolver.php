d0cc3b4a2bea908b201d5095cd4b9de73afaae08
Hardik Tankariya <hardik.tankariya@gmail.com>
2019-09-10 14:59:12 +0530

    user fix

--- app/Resolvers/SocialUserResolver.php
+++ app/Resolvers/SocialUserResolver.php
@@ -4,12 +4,28 @@ namespace App\Resolvers;
 
 use App\Models\Auth\SocialAccount;
 use App\Models\Auth\User;
+use App\Repositories\Frontend\Auth\UserRepository;
 use Coderello\SocialGrant\Resolvers\SocialUserResolverInterface;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialUserResolver implements SocialUserResolverInterface
 {
+    /**
+     * @var UserRepository
+     */
+    protected $userRepository;
+
+    /**
+     * SocialLoginController constructor.
+     *
+     * @param UserRepository  $userRepository
+     */
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     /**
      * Resolve user by provider credentials.
      *
@@ -22,12 +38,9 @@ class SocialUserResolver implements SocialUserResolverInterface
     {
         // Return the user that corresponds to provided credentials.
         // If the credentials are invalid, then return NULL.
-        $social_account = SocialAccount::where('provider','=',$provider)->where('token','=',$accessToken)->first();
-        if( $social_account ){
-            return User::find($social_account->user_id);
-        }else{
-            return NULL;
-        }
+        $account = Socialite::driver($provider)->userFromToken($accessToken);
+        $user = $this->userRepository->findOrCreateProvider($account,$provider);
+        return $user;
 
     }
 }
