55235dfac18197d2e954e622523f224c29cdc7ab
Hardik Tankariya <hardik.tankariya@gmail.com>
2019-09-17 10:22:04 +0530

    security patch fix

--- routes/backend/admin.php
+++ routes/backend/admin.php
@@ -14,12 +14,15 @@ Route::redirect('/', '/user/dashboard', 301);
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 
+Route::group(['middleware' => 'role:administrator'], function () {
+
 //===== Teachers Routes =====//
-Route::resource('teachers', 'Admin\TeachersController');
-Route::get('get-teachers-data', ['uses' => 'Admin\TeachersController@getData', 'as' => 'teachers.get_data']);
-Route::post('teachers_mass_destroy', ['uses' => 'Admin\TeachersController@massDestroy', 'as' => 'teachers.mass_destroy']);
-Route::post('teachers_restore/{id}', ['uses' => 'Admin\TeachersController@restore', 'as' => 'teachers.restore']);
-Route::delete('teachers_perma_del/{id}', ['uses' => 'Admin\TeachersController@perma_del', 'as' => 'teachers.perma_del']);
+    Route::resource('teachers', 'Admin\TeachersController');
+    Route::get('get-teachers-data', ['uses' => 'Admin\TeachersController@getData', 'as' => 'teachers.get_data']);
+    Route::post('teachers_mass_destroy', ['uses' => 'Admin\TeachersController@massDestroy', 'as' => 'teachers.mass_destroy']);
+    Route::post('teachers_restore/{id}', ['uses' => 'Admin\TeachersController@restore', 'as' => 'teachers.restore']);
+    Route::delete('teachers_perma_del/{id}', ['uses' => 'Admin\TeachersController@perma_del', 'as' => 'teachers.perma_del']);
+});
 
 
 
