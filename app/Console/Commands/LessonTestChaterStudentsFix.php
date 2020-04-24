45c2ac11f0a01224b6837a3e154eca191a37a970
Nikhil <nikhil@eyuva.com>
2020-04-24 15:14:39 +0530

    student chapter fix command

--- app/Console/Commands/LessonTestChaterStudentsFix.php
+++ app/Console/Commands/LessonTestChaterStudentsFix.php
@@ -39,12 +39,12 @@ class LessonTestChaterStudentsFix extends Command
      */
     public function handle()
     {
-        $lessons = Lesson::get();
+        $lessons = Lesson::onlyTrashed()->get();
         foreach ($lessons as $lesson){
             $lesson->chapterStudents()->where('course_id', $lesson->course_id)->forceDelete();
         }
 
-        $tests = Test::get();
+        $tests = Test::onlyTrashed()->get();
         foreach ($tests as $test){
             $tests->chapterStudents()->where('course_id', $test->course_id)->forceDelete();
         }
