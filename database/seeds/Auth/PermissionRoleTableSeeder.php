f9a7ad5d54960b97f92d657d31987547e880eb8d
Nikhil <nikhil@eyuva.com>
2020-08-22 23:53:32 +0530

    fix refresh command with permissions

--- database/seeds/Auth/PermissionRoleTableSeeder.php
+++ database/seeds/Auth/PermissionRoleTableSeeder.php
@@ -117,6 +117,18 @@ class PermissionRoleTableSeeder extends Seeder
             ['id' => 71, 'name' => 'bundle_view',],
             ['id' => 72, 'name' => 'bundle_delete',],
 
+            ['id' => 73, 'name' => 'live_lesson_access'],
+            ['id' => 74, 'name' => 'live_lesson_create'],
+            ['id' => 75, 'name' => 'live_lesson_edit'],
+            ['id' => 76, 'name' => 'live_lesson_view'],
+            ['id' => 77, 'name' => 'live_lesson_delete'],
+
+            ['id' => 78, 'name' => 'live_lesson_slot_access'],
+            ['id' => 79, 'name' => 'live_lesson_slot_create'],
+            ['id' => 80, 'name' => 'live_lesson_slot_edit'],
+            ['id' => 81, 'name' => 'live_lesson_slot_view'],
+            ['id' => 82, 'name' => 'live_lesson_slot_delete'],
+
         ];
 
         foreach ($permissions as $item) {
@@ -125,7 +137,7 @@ class PermissionRoleTableSeeder extends Seeder
 
 //        $admin_permissions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67];
 
-        $teacher_permissions = [1, 21, 22, 23, 24,25, 26, 27, 28, 29,30, 31, 32, 33, 34,35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 47, 48, 49, 51, 68, 69, 70, 71, 72];
+        $teacher_permissions = [1, 21, 22, 23, 24,25, 26, 27, 28, 29,30, 31, 32, 33, 34,35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 47, 48, 49, 51, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82];
 
         $student_permission = [47];
 
