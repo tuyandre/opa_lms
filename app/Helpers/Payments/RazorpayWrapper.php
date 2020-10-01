5763a7435f8e20c67ed6714b8f5199cfc580029f
Nikhil <nikhil@eyuva.com>
2020-10-01 17:46:56 +0530

    added razorpay wrapper class

---
+++ app/Helpers/Payments/RazorpayWrapper.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Helpers\Payments;
+
+
+use Illuminate\Support\Str;
+use Razorpay\Api\Api;
+
+class RazorpayWrapper
+{
+
+    private $razorPay;
+    public function __construct()
+    {
+        $this->razorPay = new Api(config('services.razrorpay.key'),config('services.razrorpay.secret'));
+    }
+
+    public function order($currency, $amount)
+    {
+        $receiptId = Str::random(16);
+        $order = $this->razorPay->order->create(array('receipt' => $receiptId, 'amount' => $amount, 'currency' => $currency));
+        return $order['id'];
+    }
+
+    public function verifySignature($attributes)
+    {
+        try {
+            $status = $this->razorPay->utility->verifyPaymentSignature($attributes);
+            return true;
+        } catch (\Exception $e) {
+            \Log::info($e->getMessage() . ' for id = ' . auth()->user()->id);
+            return false;
+        }
+
+    }
+}
