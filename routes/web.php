65c9e91075c924f1332b42e229cd6b98c7ab1dae
Nikhil <nikhil@eyuva.com>
2020-10-31 17:56:09 +0530

    course route add subscribed middleware

--- routes/web.php
+++ routes/web.php
@@ -84,7 +84,7 @@ Route::post('newsletter/subscribe', 'Frontend\HomeController@subscribe')->name('
 
 //============Course Routes=================//
 Route::get('courses', ['uses' => 'CoursesController@all', 'as' => 'courses.all']);
-Route::get('course/{slug}', ['uses' => 'CoursesController@show', 'as' => 'courses.show']);
+Route::get('course/{slug}', ['uses' => 'CoursesController@show', 'as' => 'courses.show'])->middleware('subscribed');
 //Route::post('course/payment', ['uses' => 'CoursesController@payment', 'as' => 'courses.payment']);
 Route::post('course/{course_id}/rating', ['uses' => 'CoursesController@rating', 'as' => 'courses.rating']);
 Route::get('category/{category}/courses', ['uses' => 'CoursesController@getByCategory', 'as' => 'courses.category']);
