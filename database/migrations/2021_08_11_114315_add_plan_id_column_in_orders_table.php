f818d3dcdfdec45dab395fc174d5338a8e526123
Shraddha Chavda <shraddha.urvam@gmail.com>
2021-08-20 17:53:09 +0530

    - migration related changes

--- database/migrations/2021_08_11_114315_add_plan_id_column_in_orders_table.php
+++ database/migrations/2021_08_11_114315_add_plan_id_column_in_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddPlanIdColimnInOrdersTable extends Migration
+class AddPlanIdColumnInOrdersTable extends Migration
 {
     /**
      * Run the migrations.
