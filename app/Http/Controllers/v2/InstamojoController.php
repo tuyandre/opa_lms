4f8e04028bca070adec2effb5308977738e946a2
hemantkumawat <hemantkumawat275@gmail.com>
2021-09-14 18:16:19 +0530

    - Instamojo Web VIew Created.

--- app/Http/Controllers/v2/InstamojoController.php
+++ app/Http/Controllers/v2/InstamojoController.php
@@ -31,14 +31,14 @@ class InstamojoController extends Controller
     public function instamojoPayment($order_id)
     {
         $order = Order::query()->find($order_id);
-        /*if (is_null($order))
+        if (is_null($order))
             throw new Exception("Invalid Request");
         if ($order->status == 1)
-            throw new Exception("Payment Already Done");*/
+            throw new Exception("Payment Already Done");
 
         // $cartTotal = number_format($order->amount, 2);
-        /*if ($order->amount <= 9)
-            return redirect()->route('instamojo-payment.declined', ['message' => 'Order Amount Cannot be less then 10']);*/
+        if ($order->amount <= 9)
+            return redirect()->route('instamojo-payment.declined', ['message' => 'Order Amount Cannot be less then 10']);
         $cartdata = [
             "purpose" => "Buy Course/Bundle",
             "amount" => 10, //$order->amount,
